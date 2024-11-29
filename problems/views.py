cd5d783fd353704a17808e3f49868e0aaca6d242
unknown <garrett.sidle@gmail.com>
2024-11-29 17:57:44 -0500

    added basic problem views

--- problems/views.py
+++ problems/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 
 def problem(request):
-    print("test")
+    if request.method != "GET":
+        return JsonResponse({"error": "Invalid HTTP method."}, status=405)
 def problem_statistics(request):
-    print("test")
+    if request.method != "GET":
+        return JsonResponse({"error": "Invalid HTTP method."}, status=405)
 def user_problem_statistics(request):
-    print("test")
+    if request.method != "GET":
+        return JsonResponse({"error": "Invalid HTTP method."}, status=405)
