29a5324cc2d428fd6fc72610d3deca7d990f44d4
unknown <garrett.sidle@gmail.com>
2024-12-03 18:26:08 -0500

    added problem get api

--- problems/views.py
+++ problems/views.py
@@ -2,18 +2,28 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Problem
 from django.http import JsonResponse, HttpRequest 
+import json
 
 
 def problem(request : HttpRequest):
     if request.method != "GET":
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
+    problem = Problem.get_random_problem() 
     return JsonResponse({
         'message': 'Login successful',
-        'session_key': Problem.get_random_problem() 
+        'problem': {
+            'problemNo' : problem.number,
+            'original-text': problem.original_code_block,
+           'modified-text' : problem.modified_code_block
+        }
         }, status=200)
+    
+    
 def problem_statistics(request : HttpRequest):
     if request.method != "GET":
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
+    
+    
 def user_problem_statistics(request : HttpRequest):
     if request.method != "GET":
         return JsonResponse({"error": "Invalid HTTP method."}, status=405)
